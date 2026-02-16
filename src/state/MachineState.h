8e00e89e936bba1d689be07d3f8d9770eaef42fc
Kre0ns <yonpun13@gmail.com>
2026-02-16 20:51:09 +0200

    refactor: MachineState constructor and constants

--- src/state/MachineState.h
+++ src/state/MachineState.h
@@ -6,12 +6,12 @@
 
 struct MachineState
 {
-    static constexpr std::size_t DefaultTapeSize = 30000;
+    static constexpr std::size_t TapeSize = 30000;
     static constexpr std::uint8_t InitialCellValue = 0;
     static constexpr std::size_t InitialPointer = 0;
     
     std::vector<std::uint8_t> tape;
     std::size_t pointer;
 
-    MachineState(std::size_t tapeSize = DefaultTapeSize);
+    MachineState();
 };
